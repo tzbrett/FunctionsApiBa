0e54ef8f7732c8ad83ad24831fc4cbefde0b0529
Brett A <bda_73@hotmail.com.com>
2026-02-22 18:39:19 -0800

    first commit

---
+++ HttpWebApi.cs
@@ -0,0 +1,48 @@
+using System.Net;
+using FunctionsAPI.Models.School;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace Bcit.Function;
+
+public class HttpWebApi
+{
+    private readonly ILogger<HttpWebApi> _logger;
+    private readonly SchoolContext _context;
+
+    public HttpWebApi(ILoggerFactory loggerFactory, SchoolContext context)
+    {
+        _logger = loggerFactory.CreateLogger<HttpWebApi>();
+        _context = context;
+    }
+
+
+    [Function("HttpWebApi")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Welcome to Azure Functions!");
+    }
+
+    [Function("GetStudents")]
+    public async Task<HttpResponseData> GetStudents(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "students")] HttpRequestData req)
+    {
+        _logger.LogInformation("C# HTTP GET/posts trigger function processed a request in GetStudents().");
+
+        var students = await _context.Students.ToArrayAsync();
+
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "application/json");
+
+        await response.WriteStringAsync(JsonConvert.SerializeObject(students));
+
+        return response;
+    }
+
+}
---
+++ Models/School/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FunctionsAPI.Models.School;
+
+public partial class Student
+{
+    public int StudentId { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string School { get; set; } = null!;
+}
