cc818f6bd951153cc8c4c56f69b4ddc3b2a74856
Brett A <bda_73@hotmail.com.com>
2026-02-22 19:20:56 -0800

    full-crud

--- HttpWebApi.cs
+++ HttpWebApi.cs
@@ -44,5 +44,90 @@ public class HttpWebApi
 
         return response;
     }
+    [Function("GetStudentById")]
+    public async Task<HttpResponseData> GetStudentById
+    (
+    [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "students/{id}")] HttpRequestData req,
+    int id
+    )
+    {
+    _logger.LogInformation("C# HTTP GET/posts trigger function processed a request.");
+    var student = await _context.Students.FindAsync(id);
+    if (student == null)
+    {
+        var response = req.CreateResponse(HttpStatusCode.NotFound);
+        response.Headers.Add("Content-Type", "application/json");
+        await response.WriteStringAsync("Not Found");
+        return response;
+    }
+    var response2 = req.CreateResponse(HttpStatusCode.OK);
+    response2.Headers.Add("Content-Type", "application/json");
+    await response2.WriteStringAsync(JsonConvert.SerializeObject(student));
+    return response2;
+    }
+    [Function("CreateStudent")]
+    public async Task<HttpResponseData> CreateStudent
+    (
+    [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "students")] HttpRequestData req
+    )
+    {
+    _logger.LogInformation("C# HTTP POST/posts trigger function processed a request.");
+    var student = JsonConvert.DeserializeObject<Student>(req.ReadAsStringAsync().Result!);
+    _context.Students.Add(student!);
+    await _context.SaveChangesAsync();
+    var response = req.CreateResponse(HttpStatusCode.Created);
+    response.Headers.Add("Content-Type", "application/json");
+    await response.WriteStringAsync(JsonConvert.SerializeObject(student));
+    return response;
+    }
+    [Function("UpdateStudent")]
+    public async Task<HttpResponseData> UpdateStudent
+    (
+    [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "students/{id}")] HttpRequestData req,
+    int id
+    )
+    {
+    _logger.LogInformation("C# HTTP PUT/posts trigger function processed a request.");
+    var student = await _context.Students.FindAsync(id);
+    if (student == null)
+    {
+        var response = req.CreateResponse(HttpStatusCode.NotFound);
+        response.Headers.Add("Content-Type", "application/json");
+        await response.WriteStringAsync("Not Found");
+        return response;
+    }
+    var student2 = JsonConvert.DeserializeObject<Student>(req.ReadAsStringAsync().Result!);
+    student.FirstName = student2!.FirstName;
+    student.LastName = student2.LastName;
+    student.School = student2.School;
+    await _context.SaveChangesAsync();
+    var response2 = req.CreateResponse(HttpStatusCode.OK);
+    response2.Headers.Add("Content-Type", "application/json");
+    await response2.WriteStringAsync(JsonConvert.SerializeObject(student));
+    return response2;
+    }
+    [Function("DeleteStudent")]
+    public async Task<HttpResponseData> DeleteStudent
+    (
+    [HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "students/{id}")] HttpRequestData req,
+    int id
+    )
+    {
+    _logger.LogInformation("C# HTTP DELETE/posts trigger function processed a request.");
+    var student = await _context.Students.FindAsync(id);
+    if (student == null)
+    {
+        var response = req.CreateResponse(HttpStatusCode.NotFound);
+        response.Headers.Add("Content-Type", "application/json");
+        await response.WriteStringAsync("Not Found");
+        return response;
+    }
+    _context.Students.Remove(student);
+    await _context.SaveChangesAsync();
+    var response2 = req.CreateResponse(HttpStatusCode.OK);
+    response2.Headers.Add("Content-Type", "application/json");
+    await response2.WriteStringAsync(JsonConvert.SerializeObject(student));
+    return response2;
+    }
 
 }
